f070e03a4286945f3290433adc688b4400b4167d
Teque5 <teque5@gmail.com>
2022-12-24 16:31:48 -0800

    day10 neat challenge

--- 2022/src/day10.rs
+++ 2022/src/day10.rs
@@ -6,7 +6,7 @@ use ndarray::Array2;
 use ndarray::ArrayViewMut2;
 
 /// follow path of rope with head and tail
-fn part(filename: &str, part2: bool) -> isize {
+fn part1(filename: &str) -> isize {
     // read the nonsense
     let lines = common::read_lines(filename);
     let mut bleeps: Vec<String> = Vec::new();
@@ -28,6 +28,7 @@ fn part(filename: &str, part2: bool) -> isize {
     let mut cycle = 1;
     let mut wait = 0;
     let mut next = 0isize;
+
     while ldx != len {
         if wait > 0 {
             // cooldown
@@ -58,21 +59,98 @@ fn part(filename: &str, part2: bool) -> isize {
     return acc
 }
 
+fn part2(filename: &str) {
+    // read the nonsense
+    let lines = common::read_lines(filename);
+    let mut bleeps: Vec<String> = Vec::new();
+    let mut values: Vec<isize> = Vec::new();
+    //let mut screen: Vec<char> = Vec::with_capacity(6*40);
+    let mut screen = vec!['.'; 6*40];
+    let sprite = Vec::from("###.....................................");
+    let mut len: usize = 0;
+    for line in lines.iter() {
+        let mut parts = line.trim().split_whitespace();
+        bleeps.push(parts.next().unwrap().to_string());
+        match parts.next() {
+            Some(value) => values.push(value.parse::<isize>().unwrap()),
+            None => values.push(0),
+        };
+        len += 1;
+    }
+    let mut reg = 1isize; // register
+    let mut acc = 0isize; // output accumulator
+
+    let mut ldx = 0;
+    let mut cycle = 1;
+    let mut wait = 0;
+    let mut next = 0isize;
+
+    let mut position = 0isize; // position on the screen
+
+
+    while ldx != len {
+        if wait > 0 {
+            // cooldown
+            wait -= 1;
+        } else {
+            reg += next;
+            // add to register and setup next
+            if bleeps[ldx] == "addx" {
+                // addx
+                wait = 1;
+                next = values[ldx];
+            } else {
+                // noop
+                wait = 0;
+                next = 0;
+            }
+            ldx += 1;
+        }
+        //println!("{:04} reg={:5} acc={} ({} {})", cycle, reg, acc, bleeps[ldx], values[ldx]);
+        if (cycle == 20) || (cycle > 30 && (cycle+20) % 40 == 0) {
+            acc += reg * cycle as isize;
+            //println!("{} {}", cycle, reg * cycle)
+        }
+        position = (cycle-1) % 240;
+        //print!("cyc{:3} {:3} {:3}", cycle, position, reg);
+        if ((position-1) % 40 == reg) || ((position) % 40 == reg) || ((position+1) % 40 == reg) {
+            screen[position as usize] = '#';
+            //println!("  #");
+        } else {
+            screen[position as usize] = '.';
+            //println!("  .");
+        }
+        cycle += 1;
+    }
+
+    // draw screen
+    for rdx in 0..6 {
+        for cdx in 0..40 {
+            print!("{}", screen[rdx*40+cdx])
+        }
+        println!();
+    }
+}
+
 
 pub fn solve() {
     let pdx = 10;
 
     // validate each solver, then apply to test
     assert_eq!(
-        part(&format!("input/{:02}_train", pdx), false),
+        part1(&format!("input/{:02}_train", pdx)),
         common::read_lines_as::<isize>(&format!("input/{:02}_val1", pdx))[0]
     );
-    println!("Part1: {}", part(&format!("input/{:02}_test", pdx), false));
+    println!("Part1: {}", part1(&format!("input/{:02}_test", pdx)));
     /*
     assert_eq!(
-        part(&format!("input/{:02}_train2", pdx), true),
-        common::read_lines_as::<i32>(&format!("input/{:02}_val2", pdx))[0]
+        part2(&format!("input/{:02}_train2", pdx), true),
+        common::read_lines_as::<isize>(&format!("input/{:02}_val2", pdx))[0]
     );
+    */
+    part2(&format!("input/{:02}_train", pdx));
+    part2(&format!("input/{:02}_test", pdx));
+    /*
     println!("Part2: {}", part(&format!("input/{:02}_test", pdx), true));
     */
 }
