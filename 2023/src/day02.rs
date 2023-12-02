08ed7b0de635248de871eccd038b59448d3af87d
Teque5 <teque5@gmail.com>
2023-12-01 22:41:13 -0800

    day02part2 ok

--- 2023/src/day02.rs
+++ 2023/src/day02.rs
@@ -1,31 +1,19 @@
 #[path = "common.rs"]
 mod common;
+use std::cmp;
 
-// fn part1(filename: &str) -> u64 {
-//     let (mut square, dim) = common::read_lines_2d(filename);
-//     let dim = 3usize;
-//     square[(0, 0)] = '7';
-//     for rdx in 0..dim {
-//         for cdx in 0..dim {
-//             // println!("a");
-//             println!("{}", square[(rdx, cdx)]);
-//         }
-//     }
-//     return 1;
-// }
-
-fn part1(filename: &str) -> u64 {
-    let mut acc = 0u64;
+fn part1(filename: &str) -> usize {
+    let mut acc = 0usize;
 
     let lines = common::read_lines(filename);
     for line in lines.iter() {
         let mut fair_game = true;
-        let mut id = 0u64;
+        let mut id = 0usize;
         let mut position = 0usize;
         // retrieve the game ID
         for (idx, c) in line.chars().enumerate() {
             if let Some(n) = c.to_digit(10) {
-                id = id * 10 + n as u64;
+                id = id * 10 + n as usize;
                 position = idx + 2;
             } else if id > 0 {
                 // non-digit character found
@@ -43,11 +31,7 @@ fn part1(filename: &str) -> u64 {
                 let mut val_and_color = grab.trim().split_whitespace();
                 if let Some(number_str) = val_and_color.next() {
                     if let Ok(number) = number_str.parse::<isize>() {
-                        // println!("Number: {}", number);
-
-                        // The variable 'color' now contains the remaining part of the string
                         if let Some(color) = val_and_color.next() {
-                            // println!("Color: {}", color);
                             match color {
                                 "red" => red -= number,
                                 "green" => green -= number,
@@ -71,22 +55,66 @@ fn part1(filename: &str) -> u64 {
     return acc;
 }
 
-// fn part2(filename: &str) -> u64 {
+fn part2(filename: &str) -> isize {
+    let mut acc = 0isize;
 
-// }
+    let lines = common::read_lines(filename);
+    for line in lines.iter() {
+        let mut id = 0u64;
+        let mut position = 0usize;
+        // retrieve the game ID
+        for (idx, c) in line.chars().enumerate() {
+            if let Some(n) = c.to_digit(10) {
+                id = id * 10 + n as u64;
+                position = idx + 2;
+            } else if id > 0 {
+                // non-digit character found
+                break;
+            }
+        }
+        let mut red = 0isize;
+        let mut green = 0isize;
+        let mut blue = 0isize;
+
+        let pulls = line[position..].split(";").collect::<Vec<_>>();
+        for pull in pulls {
+            // for each pull, cannot exceed this quantity
+            let grabs = pull.split(",").collect::<Vec<_>>();
+            for grab in grabs {
+                let mut val_and_color = grab.trim().split_whitespace();
+                if let Some(number_str) = val_and_color.next() {
+                    if let Ok(number) = number_str.parse::<isize>() {
+                        if let Some(color) = val_and_color.next() {
+                            match color {
+                                "red" => red = cmp::max(red, number),
+                                "green" => green = cmp::max(green, number),
+                                "blue" => blue = cmp::max(blue, number),
+                                _ => println!("fuck"),
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // println!("{} power {}", id, red * green * blue);
+        acc += red * green * blue;
+    }
+
+    return acc;
+}
 
 pub fn solve() {
     // Test part-1 solver, then apply to real input.
     assert_eq!(
         part1("input/02_train"),
-        common::read_lines_as::<u64>("input/02_val1")[0]
+        common::read_lines_as::<usize>("input/02_val1")[0]
     );
     println!("Part1: {}", part1("input/02_test"));
 
-    // // Test part-2 solver, then apply to real input.
-    // assert_eq!(
-    //     part2("input/02_train"),
-    //     common::read_lines_as::<u64>("input/02_val2")[0]
-    // );
-    // println!("Part2: {}", part2("input/02_test"));
+    // Test part-2 solver, then apply to real input.
+    assert_eq!(
+        part2("input/02_train"),
+        common::read_lines_as::<isize>("input/02_val2")[0]
+    );
+    println!("Part2: {}", part2("input/02_test"));
 }
