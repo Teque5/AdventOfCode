c8bbbc69120af4e5db8d50a1ba7366a9229f4325
Teque5 <teque5@gmail.com>
2023-12-08 22:34:30 -0800

    day09part1 messy but ok!

--- 2023/src/day09.rs
+++ 2023/src/day09.rs
@@ -1,14 +1,46 @@
 #[path = "common.rs"]
 mod common;
 
-fn part(filename: &str, is_part1: bool) -> usize {
-    let mut acc = 0usize;
+fn diff(ray: Vec<isize>) -> Vec<isize> {
+    ray.windows(2).map(|pair| pair[1] - pair[0]).collect()
+}
+
+/// Oasis And Sand Instability Sensor
+fn part(filename: &str, is_part1: bool) -> isize {
+    let mut acc = 0isize;
 
     // parse info
+    let mut vals: Vec<isize> = Vec::new();
     let lines = common::read_lines(filename);
-
-
-
+    for line in lines {
+        vals = common::parse_numbers_isize(&line);
+        let mut depth = 0usize;
+        let mut some_diff = vals.clone();
+        let mut steps: Vec<isize> = Vec::new();
+        let mut endpoints: Vec<isize> = Vec::new();
+        let mut current_len = some_diff.len() - 1;
+        println!("start {:?}", vals);
+        loop {
+            // println!("pushing {}", some_diff[current_len]);
+            endpoints.push(some_diff[current_len]);
+            steps.push(some_diff[current_len] - some_diff[current_len - 1]);
+            some_diff = diff(some_diff);
+            println!("somed {:?}", some_diff);
+            // println!("steps {:?}", steps);
+            depth += 1;
+            current_len -= 1;
+            if some_diff.iter().sum::<isize>() == 0 {
+                break;
+            }
+        }
+        acc += endpoints.iter().sum::<isize>();
+        println!(
+            "acc={}   d{:?} >>> {}",
+            acc,
+            depth,
+            endpoints.iter().sum::<isize>()
+        );
+    }
 
     return acc;
 }
@@ -18,15 +50,15 @@ pub fn solve() {
     // Test part-1 solver, then apply to real input.
     assert_eq!(
         part(&format!("input/{:02}_train", day), true),
-        common::read_lines_as::<usize>(&format!("input/{:02}_val1", day))[0]
+        common::read_lines_as::<isize>(&format!("input/{:02}_val1", day))[0]
     );
     println!("Part1: {}", part(&format!("input/{:02}_test", day), true));
 
     // Test part-2 solver, then apply to real input.
     assert_eq!(
         part(&format!("input/{:02}_train", day), false),
-        common::read_lines_as::<usize>(&format!("input/{:02}_val2", day))[0]
+        common::read_lines_as::<isize>(&format!("input/{:02}_val2", day))[0]
     );
-    println!("Part2: {}", part(&format!("input/{:02}_test", day), false));
-    println!("Coded: xxx minutes");
+    // println!("Part2: {}", part(&format!("input/{:02}_test", day), false));
+    // println!("Coded: xxx minutes");
 }
