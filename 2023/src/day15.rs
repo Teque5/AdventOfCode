bf64c34acadef064fd91c69adb866a2eef5d8755
Teque5 <teque5@gmail.com>
2023-12-14 22:35:21 -0800

    tidy

--- 2023/src/day15.rs
+++ 2023/src/day15.rs
@@ -1,10 +1,14 @@
 #[path = "common.rs"]
 mod common;
 
-fn hash_char(some_char: char, current_value: &mut usize) {
-    *current_value += some_char as usize;
-    *current_value *= 17;
-    *current_value %= 256;
+fn hash_string(some_string: &String) -> usize {
+    let mut hash = 0usize;
+    for some_char in some_string.chars() {
+        hash += some_char as usize;
+        hash *= 17;
+        hash %= 256;
+    }
+    return hash;
 }
 
 /// Lens Library
@@ -12,23 +16,42 @@ fn part(filename: &str, is_part1: bool) -> usize {
     let mut acc = 0usize;
 
     // parse info
-    let lines = common::read_lines(filename);
-    for line in lines {
-        // println!("line {}", line);
-        let split_line = common::split_str_as::<String>(&line, ',');
-        // println!("enter! {:?}", split_line);
-        for step in split_line.iter() {
-            let mut charval = 0usize;
-            for c in step.chars() {
-                hash_char(c, &mut charval);
-            }
-            acc += charval;
-            // println!("{} {}", step, charval)
-        }
+    let line = common::read_lines(filename)[0].clone();
+
+    let split_line = common::split_str_as::<String>(&line, ',');
+
+    for step in split_line.iter() {
+        acc += hash_string(step);
+
     }
+
 
     return acc;
 }
+
+
+/// Lens Library
+// fn part1(filename: &str, is_part1: bool) -> usize {
+//     let mut acc = 0usize;
+
+//     // parse info
+//     let lines = common::read_lines(filename);
+//     for line in lines {
+//         // println!("line {}", line);
+//         let split_line = common::split_str_as::<String>(&line, ',');
+//         // println!("enter! {:?}", split_line);
+//         for step in split_line.iter() {
+//             let mut charval = 0usize;
+//             for c in step.chars() {
+//                 hash_char(c, &mut charval);
+//             }
+//             acc += charval;
+//             // println!("{} {}", step, charval)
+//         }
+//     }
+
+//     return acc;
+// }
 
 pub fn solve() {
     let day: usize = 15;
@@ -41,9 +64,9 @@ pub fn solve() {
 
     // Test part-2 solver, then apply to real input.
     // assert_eq!(
-    //     part(&format!("input/{:02}_train", day), false),
+    //     part2(&format!("input/{:02}_train", day), false),
     //     common::read_lines_as::<usize>(&format!("input/{:02}_val2", day))[0]
     // );
     // println!("Part2: {}", part(&format!("input/{:02}_test", day), false));
-    println!("Coded: xxx minutes");
+    println!("Coded: 20+ minutes");
 }
