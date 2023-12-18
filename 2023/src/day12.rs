edd402ffcdba1c5215c37ae3b0b1ea440144d1f5
Teque5 <teque5@gmail.com>
2023-12-18 09:19:41 -0800

    fiddle w/day12part2

--- 2023/src/day12.rs
+++ 2023/src/day12.rs
@@ -13,25 +13,26 @@ fn are_equal(alpha: &Vec<char>, omega: &Vec<char>) -> bool {
     return true;
 }
 
+/// determine how dots can be distributed between groups
 fn generate_combinations(
-    n: usize,
-    r: usize,
+    n_spots: usize,
+    r_dots: usize,
     current_combination: &mut Vec<usize>,
     all_combinations: &mut Vec<Vec<usize>>,
     current_spot: usize,
 ) {
-    if current_spot == r {
+    if current_spot == r_dots {
         // If we have filled all spots and the sum is equal to n, add the current combination to the result
-        if current_combination.iter().sum::<usize>() == n {
+        if current_combination.iter().sum::<usize>() == n_spots {
             all_combinations.push(current_combination.clone());
         }
     } else {
         // Place the ball in the current spot and proceed to the next spot
-        for balls_in_current_spot in 0..=n {
-            current_combination.push(balls_in_current_spot);
+        for dots_in_current_spot in 0..=n_spots {
+            current_combination.push(dots_in_current_spot);
             generate_combinations(
-                n,
-                r,
+                n_spots,
+                r_dots,
                 current_combination,
                 all_combinations,
                 current_spot + 1,
@@ -43,14 +44,10 @@ fn generate_combinations(
 
 // Broken Hot Springs
 fn part(filename: &str, is_part1: bool) -> usize {
-    let mut arrangements = 0usize;
     let arrangements = AtomicUsize::new(0);
-    // let all_groups: Vec<Vec<usize>> = Vec::new();
-    // let all_records: Vec<Vec<char>> = Vec::new();
     // parse info
     let lines = common::read_lines(filename);
     lines.par_iter().for_each(|line| {
-    // for line in lines {
         let mut record_and_groups = line.split_whitespace();
         let mut record = record_and_groups
             .next()
@@ -58,7 +55,6 @@ fn part(filename: &str, is_part1: bool) -> usize {
             .chars()
             .collect::<Vec<_>>();
         let mut groups = common::split_str_as::<usize>(record_and_groups.next().unwrap(), ',');
-
 
         if !is_part1 {
             // 5x everything; insert extra ? between records
@@ -71,26 +67,15 @@ fn part(filename: &str, is_part1: bool) -> usize {
             }
             record = new_record;
             groups = new_groups;
-
             // println!("x {:?}", record.clone().into_iter().collect::<String>());
             // println!("x {:?}", groups);
         }
 
         // determine possible arrangements
         let min_len = groups.iter().sum::<usize>() + groups.len() - 1;
-        // if min_len == record.len() {
-        //     // only 1 possible arrangement; isn't likely to occur
-        //     arrangements += 1;
-        //     continue;
-        // }
 
         // construct a possibility and then fuck with it
         let mut possible: Vec<char> = Vec::new();
-        // let mut stretch_spots: Vec<usize> = Vec::new();
-
-        // possible.extend(vec!['.'; record.len() - min_len]);
-        // let mut combos = Vec::new()
-        // generate_combinations(record.len() - min_len, groups.len()+1, &mut combos)
 
         // figure out where blank space could be distributed
         let mut all_combinations = Vec::new();
@@ -102,7 +87,7 @@ fn part(filename: &str, is_part1: bool) -> usize {
             &mut all_combinations,
             0,
         );
-        println!("combocalcok");
+        println!("{} combos possible", all_combinations.len());
         for combo in all_combinations.iter() {
             possible.clear();
 
@@ -113,7 +98,6 @@ fn part(filename: &str, is_part1: bool) -> usize {
             for (cdx, count) in groups.iter().enumerate() {
                 if cdx != 0 {
                     let mut dotcount = 1usize;
-                    // // possible.push('.');
                     if combo[cdx] != 0 {
                         dotcount += combo[cdx];
                     }
@@ -125,6 +109,7 @@ fn part(filename: &str, is_part1: bool) -> usize {
                 possible.extend(vec!['.'; combo[groups.len()]]);
             }
             let is_ok = are_equal(&possible, &record);
+
             if is_ok {
                 arrangements.fetch_add(1, Ordering::Relaxed);
                 println!(
@@ -135,15 +120,6 @@ fn part(filename: &str, is_part1: bool) -> usize {
                 );
             }
         }
-
-        // println!("{:?} combo{:?}", all_combinations, all_combinations.len());
-        // println!("mmm {}", are_equal(&possible, &record));
-        // println!(
-        //     "{:?}\n{:?}\n{:?}\n\n",
-        //     groups,
-        //     record.into_iter().collect::<String>(),
-        //     possible.into_iter().collect::<String>(),
-        // );
     });
 
     return arrangements.load(Ordering::Relaxed);
@@ -159,10 +135,10 @@ pub fn solve() {
     println!("Part1: {}", part(&format!("input/{:02}_test", day), true));
 
     // Test part-2 solver, then apply to real input.
-    assert_eq!(
-        part(&format!("input/{:02}_train", day), false),
-        common::read_lines_as::<usize>(&format!("input/{:02}_val2", day))[0]
-    );
+    // assert_eq!(
+    //     part(&format!("input/{:02}_train", day), false),
+    //     common::read_lines_as::<usize>(&format!("input/{:02}_val2", day))[0]
+    // );
     // println!("Part2: {}", part(&format!("input/{:02}_test", day), false));
     // println!("Coded: 120+ minutes");
 }
