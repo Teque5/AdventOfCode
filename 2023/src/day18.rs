5b4e1f008fe7b3c850523a7253c59770561d2671
Teque5 <teque5@gmail.com>
2023-12-18 00:30:46 -0800

    day18part2 skeleton code

--- 2023/src/day18.rs
+++ 2023/src/day18.rs
@@ -8,26 +8,34 @@ fn part(filename: &str, is_part1: bool) -> isize {
     let mut gridsize_pos = (0isize, 0isize);
     let mut directions: Vec<char> = Vec::new();
     let mut amounts: Vec<usize> = Vec::new();
-    let mut hexs: Vec<String> = Vec::new();
 
     // parse info
     let lines = common::read_lines(filename);
     let mut position = (0isize, 0isize);
     for line in lines {
         let mut line_split = line.split_whitespace();
-        let direction: char = line_split.next().unwrap().chars().next().unwrap();
-        directions.push(direction);
-        let amount: usize = common::parse_numbers(line_split.next().unwrap())[0];
-        amounts.push(amount);
-        let hex: String = line_split
-            .next()
-            .unwrap()
-            .to_string()
-            .replace("(", "")
-            .replace(")", "")
-            .replace("#", "");
-        hexs.push(hex.clone());
-        // println!("{} {} {}", direction, amount, hex);
+        let mut direction: char = 'z';
+        let mut amount: usize = 0;
+        if is_part1 {
+            direction = line_split.next().unwrap().chars().next().unwrap();
+            directions.push(direction);
+            amount = common::parse_numbers(line_split.next().unwrap())[0];
+            amounts.push(amount);
+        } else {
+            let rawhex: Vec<char> = line_split.nth(2).unwrap().chars().collect();
+            match rawhex.get(rawhex.len()-2).unwrap() {
+                '0' => direction = 'R',
+                '1' => direction = 'D',
+                '2' => direction = 'L',
+                '3' => direction = 'U',
+                _ => panic!("no"),
+            }
+            let hexstr: String = rawhex[2..rawhex.len()-2].iter().collect();
+            amount = usize::from_str_radix(&hexstr, 16).unwrap();
+            println!("{:?} {}", direction, amount);
+
+        }
+
         // determine grid size
         match direction {
             'U' => position.0 -= amount as isize,
@@ -48,7 +56,10 @@ fn part(filename: &str, is_part1: bool) -> isize {
     let cols: usize = (gridsize_pos.1 - gridsize_neg.1) as usize + 1;
 
     let mut position: (usize, usize) = ((-gridsize_neg.0 as usize), (-gridsize_neg.1 as usize));
-    // println!("{:?} {:?} {:?}", rows, cols, position);
+    if !is_part1 {
+        panic!("part 2 not solvable yet; cannot allocate that much memory!");
+    }
+
     // trace dig path in the grid
     let mut grid: Array2<char> = Array2::from_elem((rows, cols), '.');
     grid[position] = '#';
@@ -65,50 +76,14 @@ fn part(filename: &str, is_part1: bool) -> isize {
             grid[position] = '#';
         }
     }
-    common::print_2d_chars(&grid);
+    // common::print_2d_chars(&grid);
     // waterfill lava
     let mut filled = true;
     while filled {
         // keep calling this until waterfill is complete
         filled = waterfill(&mut grid, rows, cols);
     }
-
-    // raytrace lava
-    // for rdx in 0..rows {
-    //     let mut inside:bool = false;
-    //     for cdx in 0..cols {
-    //         // if grid[(rdx, cdx)] != '#' {
-    //         //     if cdx > 0 {
-    //         //     }
-    //         // }
-    //         //             // do not flip
-    //         //         } else {
-    //             //             println!("{} {} {}", rdx, cdx, inside);
-    //             //         }
-
-    //             //     } else {
-    //                 //         inside = !inside;
-    //                 //         println!("{} {} {}", rdx, cdx, inside);
-    //                 //     }
-
-    //         // }
-    //         if grid[(rdx, cdx)] == '#' {
-    //             lava += 1;
-    //             if cdx > 0 {
-    //                 if grid[(rdx, cdx-1)] != '#' {
-    //                     inside = !inside;
-    //                 }
-    //             } else {
-    //                 inside = !inside;
-    //             }
-    //         } else if inside {
-    //             grid[(rdx, cdx)] = 'x';
-    //             lava += 1;
-    //         }
-
-    //     }
-    // }
-    common::print_2d_chars(&grid);
+    // common::print_2d_chars(&grid);
     // count lava
     let mut lava = 0isize;
     for rdx in 0..rows {
