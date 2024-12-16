dfd8c6ec46d33fbcb8ec6b10973b82881276ef44
Teque5 <teque5@gmail.com>
2024-12-16 07:57:43 -0800

    aoc::Image implementation complete

--- 2024/src/day14.rs
+++ 2024/src/day14.rs
@@ -1,9 +1,4 @@
-use ab_glyph::{FontRef, PxScale};
 use aoc;
-use image::{Rgb, RgbImage};
-use imageproc::drawing::{draw_filled_rect_mut, draw_text_mut, text_size};
-use imageproc::rect::Rect;
-use std::process::Command;
 
 /// print robot positions to console
 #[allow(dead_code)]
@@ -15,25 +10,6 @@ fn print_robots(robots: &Vec<(i64, i64, i64, i64)>, rows: i64, cols: i64) {
     for row in &buffer {
         let row_str: String = row.iter().collect();
         println!("{}", row_str);
-    }
-}
-
-/// Update image img with new robot positions, write to file
-fn draw_robots(img: &mut RgbImage, robots: &Vec<(i64, i64, i64, i64)>) {
-    for pixel in img.pixels_mut() {
-        pixel[0] = (pixel[0] as f32 * 0.95) as u8;
-        // pixel[1] = (pixel[0] as f32 / 4.0 + pixel[1] as f32 * 0.6) as u8;
-        pixel[1] = 0;
-        pixel[2] = 0;
-    }
-    // let mut max_intensity = 0f32;
-    for robot in robots {
-        let pixel = img.get_pixel_mut(robot.1 as u32, robot.0 as u32);
-        let intensity =
-            (((robot.3 as f32).powf(2.0) + (robot.2 as f32).powf(2.0)).sqrt() * 2.0) as u8;
-        pixel[0] = intensity;
-        pixel[1] = intensity;
-        pixel[2] = intensity;
     }
 }
 
@@ -81,22 +57,15 @@ fn part(filename: &str, is_part1: bool) -> u64 {
     let is_training = robots.len() == 12;
     let rows: i64 = if is_training { 7 } else { 103 };
     let cols: i64 = if is_training { 11 } else { 101 };
-    // image related vars
 
-    let font = FontRef::try_from_slice(include_bytes!("../SNESesque.ttf")).unwrap();
-    let height = 13f32;
-    let scale = PxScale {
-        x: height,
-        y: height,
-    };
-
-    let mut img = RgbImage::new(cols as u32 + 1, rows as u32 + 1);
+    // create image to draw robots upon
+    let mut img = aoc::Image::new(rows as usize, cols as usize);
+    // img.set_fontsize(1.0); // create very small image for github
+    img.set_framerate(20);
     // simulate robots
     for second in 0..7777 {
         for rdx in 0..robots.len() {
-            // row = (row + row_vel) % rows
             robots[rdx].0 = (robots[rdx].0 + robots[rdx].2) % rows;
-            // col = (col + col_vel) % cols
             robots[rdx].1 = (robots[rdx].1 + robots[rdx].3) % cols;
             if robots[rdx].0 < 0 {
                 robots[rdx].0 += rows;
@@ -109,49 +78,34 @@ fn part(filename: &str, is_part1: bool) -> u64 {
             // 99 is the 100th second!
             return safety_factor(&robots, rows, cols);
         }
-        // save frames for GIF
+        // render frames for GIF
         if second % 101 == 97 {
-            let mut filename = format!("day14_{:05}.png", second);
             let safety = safety_factor(&robots, rows, cols);
-            let txt = &format!("safety={}", safety);
-            let txt_size = text_size(scale, &font, txt);
-            draw_robots(&mut img, &robots);
-            draw_filled_rect_mut(
-                &mut img,
-                Rect::at(0, 2).of_size(txt_size.0, txt_size.1),
-                Rgb([0, 0, 0]),
-            );
-            draw_text_mut(&mut img, Rgb([255, 255, 255]), 0, 0, scale, &font, txt);
-            img.save(filename).expect("save failed");
-            // print_robots(&robots, rows, cols);
-            if second == 7773 {
-                for idx in second + 1..second + 30 {
-                    filename = format!("day14_{:05}.png", idx);
-                    draw_robots(&mut img, &robots);
-                    draw_text_mut(&mut img, Rgb([255, 255, 255]), 0, 0, scale, &font, txt);
-                    img.save(filename).expect("save failed");
-                }
+            let safety_txt = format!("safety = {}", safety);
+            let frame_txt = format!("second = {}", second);
+            for robot in &robots {
+                img.draw_bool(robot.0 as usize, robot.1 as usize, true);
             }
+            img.draw_text(0, 0, &safety_txt);
+            img.draw_text(1, 0, &frame_txt);
+            img.render_frame();
+            // render final GIF
+            if second == 7773 {
+                for _ in 0..15 {
+                    // render extra fade-out frames w/ robots in current position
+                    for robot in &robots {
+                        img.draw_bool(robot.0 as usize, robot.1 as usize, true);
+                    }
+                    img.render_frame();
+                    // img.save_frame(&"out.png");
+                    img.fade()
+                }
+                img.render_gif(&"img/day14.gif");
+                // img.render_webp(&"img/day14.webp");
+            }
+            img.fade();
         }
     }
-    // compile frames to GIF
-    Command::new("ffmpeg")
-        .args([
-            "-y",
-            "-pattern_type",
-            "glob",
-            "-i",
-            "day14*.png",
-            "img/day14.gif",
-        ])
-        .output()
-        .expect("ffmpeg merge failed");
-    // delete raw frames
-    Command::new("sh")
-        .args(["-c", "rm day14*.png"])
-        .output()
-        .expect("error during rm");
-
     return 0;
 }
 
--- 2024/src/lib.rs
+++ 2024/src/lib.rs
@@ -1,14 +1,17 @@
 /// Library functions for Advent of Code solutions
 /// Copyright 2021 Alex Utter, 2022-2024 Teque5
-// use ab_glyph::{FontRef, PxScale}};
-// use image::RgbImage;
+use ab_glyph::{FontRef, PxScale};
+use image::{Rgb, RgbImage};
+use imageproc::drawing::draw_text_mut;
 use ndarray::{Array2, Axis};
 use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 use std::io::Read;
 use std::io::Result;
+use std::process::Command;
 use std::str::FromStr;
+use tempfile::{tempdir, TempDir};
 
 /// commonly used directions (row, col) for mazes and whatnot
 pub const DIRECTIONS: [(isize, isize); 4] = [
@@ -108,4 +111,264 @@ pub fn parse_numbers<T: FromStr>(line: &str) -> Vec<T> {
         .filter(|s| !s.is_empty())
         .filter_map(|s| s.parse::<T>().ok())
         .collect()
+}
+
+#[allow(dead_code)]
+pub struct Image<'a> {
+    img: RgbImage,
+    font: FontRef<'a>,
+    scale: PxScale,
+    dir: TempDir,
+    rows: usize,
+    cols: usize,
+    fdx: u16,      // frame index
+    alpha: f32,    // fade alpha
+    framerate: u8, // gif framerate
+}
+
+/// Very often in AoC we have an Array2<_> where we want to visualize the changes
+/// ## Example (not doctest)
+/// let img = aoc::Image::new(10, 20);
+/// for _ in 0..10 {
+///     img.draw_chars(&some_2d_array);
+///     img.render_frame()
+///     img.fade()
+/// }
+/// img.render_gif("out.gif");
+#[allow(dead_code)]
+impl Image<'_> {
+    /// create new image surface
+    pub fn new(rows: usize, cols: usize) -> Self {
+        // font needs to be monospace
+        let font = FontRef::try_from_slice(include_bytes!("../../unscii-8.otf")).unwrap();
+        let scale = PxScale {
+            x: 8.0, // fixed for unscii-8
+            y: 8.0, // fixed for unscii-8
+        };
+        let img = RgbImage::new(
+            (scale.x as usize * cols) as u32,
+            (scale.y as usize * rows) as u32,
+        );
+        // TODO: handle tempdir()? instead of unwrapping
+        let dir = tempdir().unwrap();
+        Image {
+            img: img,
+            font: font,
+            scale: scale,
+            dir: dir,
+            rows: rows,
+            cols: cols,
+            fdx: 0,
+            alpha: 0.05,
+            framerate: 15,
+        }
+        // TODO: check that ffmpeg is installed
+    }
+    /// clear image and redefine character size
+    /// 8 (default) is pixel-perfect for text
+    /// 4 is barely resolvable
+    /// 1 is for tiny pixel plotting using draw_bool
+    pub fn set_fontsize(&mut self, size_px: f32) {
+        self.scale.x = size_px;
+        self.scale.y = size_px;
+        self.img = RgbImage::new(
+            (self.scale.x as usize * self.cols) as u32,
+            (self.scale.y as usize * self.rows) as u32,
+        );
+    }
+
+    // drawing subroutines
+
+    /// fade every pixel to emphasize any new frames
+    pub fn fade(&mut self) {
+        // fade old image into red channel
+        for pixel in self.img.pixels_mut() {
+            pixel[0] = (pixel[0] as f32 * (1.0 - self.alpha)) as u8;
+            pixel[1] = 0;
+            pixel[2] = 0;
+        }
+    }
+
+    /// draw exactly 1 or more chars at (row, col)
+    pub fn draw_text(&mut self, row: usize, col: usize, txt: &str) {
+        let y_offset = (row as f32 * self.scale.y) as i32;
+        let x_offset = (col as f32 * self.scale.y) as i32;
+        // draw black background (not really needed)
+        // let blank: String = "█".repeat(txt.len());
+        // draw_text_mut(
+        //     &mut self.img,
+        //     Rgb([0, 0, 0]),
+        //     x_offset,
+        //     y_offset,
+        //     self.scale,
+        //     &self.font,
+        //     &blank,
+        // );
+        draw_text_mut(
+            &mut self.img,
+            Rgb([255, 255, 255]),
+            x_offset,
+            y_offset,
+            self.scale,
+            &self.font,
+            txt,
+        );
+    }
+
+    /// draw a full or empty value at (row, col)
+    pub fn draw_bool(&mut self, row: usize, col: usize, value: bool) {
+        let y_offset = (row as f32 * self.scale.y) as i32;
+        let x_offset = (col as f32 * self.scale.y) as i32;
+        let color = if value {
+            Rgb([255, 255, 255])
+        } else {
+            Rgb([0, 0, 0])
+        };
+        draw_text_mut(
+            &mut self.img,
+            color,
+            x_offset,
+            y_offset,
+            self.scale,
+            &self.font,
+            &'█'.to_string(),
+        );
+    }
+
+    /// draw full image from Array2<bool>
+    pub fn draw_bools(&mut self, ray: &Array2<bool>) {
+        for ((row, col), value) in ray.indexed_iter() {
+            self.draw_bool(row, col, *value);
+        }
+    }
+
+    /// draw full image from Array2<char>
+    pub fn draw_chars(&mut self, ray: &Array2<char>) {
+        // draw new image row-by-row
+        for (rdx, row) in ray.rows().into_iter().enumerate() {
+            let full_row: String = row.iter().collect();
+            let y_offset = (rdx as f32 * self.scale.y) as i32;
+            draw_text_mut(
+                &mut self.img,
+                Rgb([255, 255, 255]),
+                0,
+                y_offset,
+                self.scale,
+                &self.font,
+                &full_row,
+            );
+        }
+    }
+
+    // setters & getters
+
+    /// larger alpha -> shorter fade-out per frame
+    pub fn set_alpha(&mut self, new_alpha: f32) {
+        if new_alpha > 0.0 && new_alpha < 1.0 {
+            self.alpha = new_alpha;
+        } else {
+            println!("err alpha {} out of range (0, 1)", new_alpha);
+        }
+    }
+
+    pub fn set_framerate(&mut self, new_framerate: u8) {
+        self.framerate = new_framerate;
+    }
+
+    // frame rendering
+
+    /// save current frame to specific file
+    pub fn save_frame(&mut self, file_path: &str) {
+        self.img.save(file_path).expect("save failed");
+    }
+
+    /// Save current frame to temp dir
+    pub fn render_frame(&mut self) {
+        let file_path = self.dir.path().join(format!("aoc_{:05}.png", self.fdx));
+        self.img.save(file_path).expect("save failed");
+        self.fdx += 1;
+    }
+
+    /// write framed to animated webp
+    pub fn render_webp(&self, file_path: &str) {
+        let glob_path = format!("{}", self.dir.path().join("aoc_*.png").display());
+        Command::new("ffmpeg")
+            .args([
+                "-y",
+                "-framerate",
+                &format!("{}", self.framerate),
+                "-pattern_type",
+                "glob",
+                "-i",
+                &glob_path,
+                "-vcodec",
+                "libwebp",
+                // "-quality", // enable lossy (not usually worth it)
+                // "10",
+                "-lossless", // enable lossless
+                "1",
+                "-loop", // loop forever
+                "0",
+                "-preset",
+                "drawing",
+                file_path,
+            ])
+            .output()
+            .expect("ffmpeg failed");
+    }
+
+    /// write frames to animated GIF
+    /// for very small images the one pass approach is often better
+    pub fn render_gif(&self, file_path: &str) {
+        let glob_path = format!("{}", self.dir.path().join("aoc_*.png").display());
+        // one pass
+        Command::new("ffmpeg")
+            .args([
+                "-y",
+                "-framerate",
+                &format!("{}", self.framerate),
+                "-pattern_type",
+                "glob",
+                "-i",
+                &glob_path,
+                file_path,
+            ])
+            .output()
+            .expect("ffmpeg pass 1 failed");
+        // // two pass
+        // let pal_path = format!("{}", self.dir.path().join("palette.png").display());
+        // // Pass 1
+        // Command::new("ffmpeg")
+        //     .args([
+        //         "-y",
+        //         "-pattern_type",
+        //         "glob",
+        //         "-i",
+        //         &glob_path,
+        //         "-vf",
+        //         "palettegen",
+        //         &pal_path,
+        //     ])
+        //     .output()
+        //     .expect("ffmpeg pass 1 failed");
+
+        // // Pass 2
+        // Command::new("ffmpeg")
+        //     .args([
+        //         "-y",
+        //         "-framerate",
+        //         &format!("{}", self.framerate),
+        //         "-pattern_type",
+        //         "glob",
+        //         "-i",
+        //         &glob_path,
+        //         "-i",
+        //         &pal_path,
+        //         "-lavfi",
+        //         "paletteuse",
+        //         file_path,
+        //     ])
+        //     .output()
+        //     .expect("ffmpeg pass 2 failed");
+    }
 }
