330d7d7a4739ad709bfc4552af9594bc49321fb0
Teque5 <teque5@gmail.com>
2025-11-30 22:32:59 -0800

    day01 ok

--- 2025/src/day01.rs
+++ 2025/src/day01.rs
@@ -1,49 +1,81 @@
 use aoc;
 
-/// List Distance
+/// Dial Password, Count Zeros on Circular Dial
 fn part(filename: &str, is_part1: bool) -> usize {
     let mut acc = 0usize;
-    let mut left: Vec<isize> = Vec::new();
-    let mut right: Vec<isize> = Vec::new();
+    let mut dial = 50isize;
+    let mut prev: isize;
 
     // parse info
     let lines = aoc::read_lines(filename);
     for line in lines {
         let row = aoc::parse_numbers::<isize>(&line);
-        left.push(row[0]);
-        right.push(row[1]);
-    }
-    if is_part1 {
-        // between left and right, sum distances between lists
-        // sort lists
-        left.sort();
-        right.sort();
-        // sum up distances
-        for idx in 0..left.len() {
-            acc += (left[idx] - right[idx]).abs() as usize;
+        // set left true if first char is L
+        let left = line.chars().next().unwrap() == 'L';
+        // println!("row: {:?} left: {}", row, left);
+        prev = dial;
+        if left {
+            dial -= row[0];
+        } else {
+            dial += row[0];
         }
-    } else {
-        // count how many times left number appears in right list; do multiply later
-        for &lval in left.iter() {
-            let mut subcount = 0usize;
-            for &rval in right.iter() {
-                if lval == rval {
-                    subcount += 1;
+        if is_part1 {
+            while dial < 0 {
+                dial += 100;
+            }
+            while dial >= 100 {
+                dial -= 100;
+            }
+            // println!("{:<3}  dial: {}", line, dial);
+            if dial == 0 {
+                acc += 1;
+            }
+        } else {
+            loop {
+                // println!(" prev: {}", prev);
+
+                if dial == 0 {
+                    // tick at zero
+                    acc += 1;
+                    // print!("*");
+                    break;
+                } else if dial > 0 && dial < 100 {
+                    // ok
+                    break;
+                } else if dial < -100 {
+                    // must cross zero once
+                    dial += 100;
+                    acc += 1;
+                    // print!("-");
+                } else if dial < 0 {
+                    dial += 100;
+                    if prev != 0 {
+                        acc += 1;
+                        // print!("-");
+                    }
+                } else if dial > 100 {
+                    // must cross zero once
+                    dial -= 100;
+                    acc += 1;
+                    // print!("+");
+                } else if dial == 100 {
+                    dial -= 100;
                 }
             }
-            acc += subcount * (lval as usize);
         }
+        // println!("{:<3}  dial: {}", line, dial);
     }
+    // not 6392, 6291, 5396 too low, not 5828
     return acc;
 }
 
 /// Check training data, then apply to test data
 pub fn solve(day: usize) {
-    assert_eq!(part(&format!("input/{:02}_train", day), true), 11);
-    // println!("Part1: {}", part(&format!("input/{:02}_test", day), true));
+    assert_eq!(part(&format!("input/{:02}_train", day), true), 3);
+    println!("Part1: {}", part(&format!("input/{:02}_test", day), true));
 
-    // assert_eq!(part(&format!("input/{:02}_train", day), false), 31);
-    // println!("Part2: {}", part(&format!("input/{:02}_test", day), false));
+    assert_eq!(part(&format!("input/{:02}_train", day), false), 6);
+    println!("Part2: {}", part(&format!("input/{:02}_test", day), false));
 
-    println!("Coded: 45 minutes");
+    println!("Coded: 88 minutes; out of practice.");
 }
