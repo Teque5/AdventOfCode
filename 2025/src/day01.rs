4b4f0f80df9ec5af92cd206f2e87a588916b1a17
Teque5 <teque5@gmail.com>
2025-11-30 18:51:16 -0800

    prep for 2025

---
+++ 2025/src/day01.rs
@@ -0,0 +1,49 @@
+use aoc;
+
+/// List Distance
+fn part(filename: &str, is_part1: bool) -> usize {
+    let mut acc = 0usize;
+    let mut left: Vec<isize> = Vec::new();
+    let mut right: Vec<isize> = Vec::new();
+
+    // parse info
+    let lines = aoc::read_lines(filename);
+    for line in lines {
+        let row = aoc::parse_numbers::<isize>(&line);
+        left.push(row[0]);
+        right.push(row[1]);
+    }
+    if is_part1 {
+        // between left and right, sum distances between lists
+        // sort lists
+        left.sort();
+        right.sort();
+        // sum up distances
+        for idx in 0..left.len() {
+            acc += (left[idx] - right[idx]).abs() as usize;
+        }
+    } else {
+        // count how many times left number appears in right list; do multiply later
+        for &lval in left.iter() {
+            let mut subcount = 0usize;
+            for &rval in right.iter() {
+                if lval == rval {
+                    subcount += 1;
+                }
+            }
+            acc += subcount * (lval as usize);
+        }
+    }
+    return acc;
+}
+
+/// Check training data, then apply to test data
+pub fn solve(day: usize) {
+    assert_eq!(part(&format!("input/{:02}_train", day), true), 11);
+    // println!("Part1: {}", part(&format!("input/{:02}_test", day), true));
+
+    // assert_eq!(part(&format!("input/{:02}_train", day), false), 31);
+    // println!("Part2: {}", part(&format!("input/{:02}_test", day), false));
+
+    println!("Coded: 45 minutes");
+}
---
+++ 2025/src/main.rs
@@ -0,0 +1,87 @@
+/// Top-level dispatcher for Advent Of Code 2024 solutions
+/// https://adventofcode.com/
+/// To run a given day's solution: cargo run [day#]
+/// To run all day's solutions: cargo run all
+
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashMap;
+use std::env;
+use std::time;
+mod fetch;
+
+mod day01;
+// mod day02;
+// mod day03;
+// mod day04;
+// mod day05;
+// mod day06;
+// mod day07;
+// mod day08;
+// mod day09;
+// mod day10;
+// mod day11;
+// mod day12;
+
+// List of functions, one for each day's problem
+lazy_static! {
+    static ref SOLUTIONS: HashMap<usize, fn(usize)> = vec![
+        (1, day01::solve as _),
+        // (2, day02::solve as _),
+        // (3, day03::solve as _),
+        // (4, day04::solve as _),
+        // (5, day05::solve as _),
+        // (6, day06::solve as _),
+        // (7, day07::solve as _),
+        // (8, day08::solve as _),
+        // (9, day09::solve as _),
+        // (10, day10::solve as _),
+        // (11, day11::solve as _),
+        // (12, day12::solve as _),
+    ].into_iter().collect();
+
+    static ref EMPTY_STRING: String = String::from("");
+}
+
+/// Wrapper that adds elapsed-time telemetry.
+fn solve_timer(day: &usize) -> usize {
+    if let Some(solver) = SOLUTIONS.get(day) {
+        println!("Day {}", day);
+        let timer = time::Instant::now();
+        solver(*day); // Run designated function
+        let elapsed = timer.elapsed().as_micros();
+        println!("Timed: {} µs", elapsed);
+        println!("");
+        elapsed as usize
+    } else {
+        eprintln!("No solution for Day {}.", day);
+        0
+    }
+}
+
+/// Main entry point looks at command-line arguments.
+fn main() {
+    let year: usize = 2025;
+    let args: Vec<String> = env::args().collect();
+    let arg = args.last().unwrap_or(&EMPTY_STRING);
+
+    if arg == "all" {
+        // Solve every problem in the list.
+        let mut elapsed = 0usize;
+        let mut keys: Vec<&usize> = SOLUTIONS.keys().collect();
+        keys.sort(); // Print in order...
+        for day in keys.iter() {
+            fetch::get_data(&year, &day);
+            elapsed += solve_timer(&day);
+        }
+        println!("Advent of Code {}", year);
+        println!("Total Runtime = {:.3} s", elapsed as f32 / 1e6);
+    } else if let Result::Ok(day) = arg.parse::<usize>() {
+        fetch::get_data(&year, &day);
+        solve_timer(&day);
+    } else {
+        eprintln!("Usage: 'cargo run [day#]' or 'cargo run all'");
+        eprintln!("  Where day# is the problem to be solved (1-25)");
+    }
+}
