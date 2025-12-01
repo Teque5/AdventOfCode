73abc92aea730a3cec0f32289b6e04d7a3d745f9
Kyle A Logue <kyle.a.logue@aero.org>
2025-12-01 10:21:07 -0800

    day01 render

--- 2025/src/day01.rs
+++ 2025/src/day01.rs
@@ -1,81 +1,81 @@
 use aoc;
 
 /// Dial Password, Count Zeros on Circular Dial
-fn part(filename: &str, is_part1: bool) -> usize {
-    let mut acc = 0usize;
-    let mut dial = 50isize;
-    let mut prev: isize;
+fn part(filename: &str, is_part1: bool, render: bool) -> usize {
+    let mut acc = 0usize; // zero count
+    let mut dial = 50isize; // dial position
+    let mut turn: isize; // change amount
+    let mut direction: isize; // change direction
+    let mut img = aoc::Image::new(1, 100);
+    img.set_frameskip(5);
+
+    if render {
+        // fancy plot for part 2
+        img.draw_bool(0, dial as usize, true);
+        img.draw_text(0, 0, &format!("Zeros: {}", acc));
+        img.render_frame();
+        img.info();
+    }
 
     // parse info
     let lines = aoc::read_lines(filename);
     for line in lines {
-        let row = aoc::parse_numbers::<isize>(&line);
-        // set left true if first char is L
-        let left = line.chars().next().unwrap() == 'L';
-        // println!("row: {:?} left: {}", row, left);
-        prev = dial;
-        if left {
-            dial -= row[0];
+        turn = aoc::parse_numbers::<isize>(&line)[0];
+        if line.chars().next().unwrap() == 'L' {
+            direction = -1;
         } else {
-            dial += row[0];
+            direction = 1;
         }
         if is_part1 {
+            dial += turn * direction;
             while dial < 0 {
                 dial += 100;
             }
             while dial >= 100 {
                 dial -= 100;
             }
-            // println!("{:<3}  dial: {}", line, dial);
             if dial == 0 {
                 acc += 1;
             }
         } else {
-            loop {
-                // println!(" prev: {}", prev);
-
+            for _ in 0..turn {
+                dial += direction;
+                if dial < 0 {
+                    dial = 99;
+                } else if dial >= 100 {
+                    dial = 0;
+                }
                 if dial == 0 {
-                    // tick at zero
                     acc += 1;
-                    // print!("*");
-                    break;
-                } else if dial > 0 && dial < 100 {
-                    // ok
-                    break;
-                } else if dial < -100 {
-                    // must cross zero once
-                    dial += 100;
-                    acc += 1;
-                    // print!("-");
-                } else if dial < 0 {
-                    dial += 100;
-                    if prev != 0 {
-                        acc += 1;
-                        // print!("-");
-                    }
-                } else if dial > 100 {
-                    // must cross zero once
-                    dial -= 100;
-                    acc += 1;
-                    // print!("+");
-                } else if dial == 100 {
-                    dial -= 100;
+                }
+                if render {
+                    img.fade();
+                    img.draw_bool(0, dial as usize, true);
+                    img.draw_text(0, 0, &format!("Zeros: {}", acc));
+                    img.render_frame();
                 }
             }
         }
-        // println!("{:<3}  dial: {}", line, dial);
     }
-    // not 6392, 6291, 5396 too low, not 5828
+
+    if render {
+        for _ in 0..5*15 {
+            img.fade();
+            img.draw_text(0, 0, &format!("Zeros: {}", acc));
+            img.render_frame();
+        }
+        img.render_webp(&"img/day01.webp");
+    }
     return acc;
 }
 
 /// Check training data, then apply to test data
 pub fn solve(day: usize) {
-    assert_eq!(part(&format!("input/{:02}_train", day), true), 3);
-    println!("Part1: {}", part(&format!("input/{:02}_test", day), true));
+    assert_eq!(part(&format!("input/{:02}_train", day), true, false), 3);
+    println!("Part1: {}", part(&format!("input/{:02}_test", day), true, false));
 
-    assert_eq!(part(&format!("input/{:02}_train", day), false), 6);
-    println!("Part2: {}", part(&format!("input/{:02}_test", day), false));
+    assert_eq!(part(&format!("input/{:02}_train", day), false, true), 6);
+    println!("Part2: {}", part(&format!("input/{:02}_test", day), false, false));
 
     println!("Coded: 88 minutes; out of practice.");
 }
