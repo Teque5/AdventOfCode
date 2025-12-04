00093e9f96354857154702a96a536aa2a03e2e73
Teque5 <teque5@gmail.com>
2025-12-03 19:25:35 -0800

    prep for day04

--- 2025/src/main.rs
+++ 2025/src/main.rs
@@ -14,7 +14,7 @@ mod fetch;
 mod day01;
 mod day02;
 mod day03;
-// mod day04;
+mod day04;
 // mod day05;
 // mod day06;
 // mod day07;
@@ -30,7 +30,7 @@ lazy_static! {
         (1, day01::solve as _),
         (2, day02::solve as _),
         (3, day03::solve as _),
-        // (4, day04::solve as _),
+        (4, day04::solve as _),
         // (5, day05::solve as _),
         // (6, day06::solve as _),
         // (7, day07::solve as _),
