1182c1c4d4e4ff71eb897f743203d3ffa4a48e8c
Teque5 <teque5@gmail.com>
2025-12-10 09:29:14 -0800

    refactor w/distance sorting

--- 2025/src/day08.rs
+++ 2025/src/day08.rs
@@ -1,6 +1,4 @@
 use aoc;
-// use std::collections::HashMap;
-use indicatif::{ProgressBar, ProgressStyle};
 use std::collections::HashSet;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -19,97 +17,36 @@ impl Position {
     }
 }
 
-/// Playground
-fn part1(filename: &str) -> usize {
-    let mut positions = Vec::<Position>::new();
-    let mut circuits = Vec::<isize>::new();
-    // parse
-    let lines = aoc::read_lines(filename);
-    for line in lines {
-        let xyz = aoc::parse_delimited::<i32>(&line, ',');
-        let pos = Position {
-            x: xyz[0],
-            y: xyz[1],
-            z: xyz[2],
-        };
-        positions.push(pos);
-        circuits.push(-1);
-        // println!("{} {:?}", positions.len() - 1, pos);
-    }
-
-    let mut next_circuit = 0isize;
-    let mut pairs = HashSet::<(usize, usize)>::new();
-    let num_connections = if filename.contains("train") { 10 } else { 1000 };
-    let progress = ProgressBar::new(num_connections as u64);
-    progress.set_style(ProgressStyle::with_template(aoc::STYLE).unwrap());
-    for _ in 0..num_connections {
-        let mut min_dist = std::f64::MAX;
-        let mut min_pair = (0usize, 0usize);
-        for ldx in 0..positions.len() {
-            for rdx in 0..positions.len() {
-                // println!("Considering pair: {} and {}", ldx, rdx);
-                if ldx <= rdx {
-                    continue;
-                }
-                if pairs.contains(&(ldx, rdx)) {
-                    continue;
-                }
-                if pairs.contains(&(rdx, ldx)) {
-                    continue;
-                }
-                let dist = positions[ldx].distance(&positions[rdx]);
-                // println!("Distance between {} and {}: {}", ldx, rdx, dist);
-                if dist < min_dist {
-                    min_dist = dist;
-                    min_pair = (ldx, rdx);
-                }
+///
+fn merge_circuits(circuits: &mut Vec<isize>, next_circuit: &mut isize, min_pair: (usize, usize)) {
+    let left_circuit = circuits[min_pair.0];
+    let right_circuit = circuits[min_pair.1];
+    if left_circuit == -1 && right_circuit == -1 {
+        // New circuit
+        circuits[min_pair.0] = *next_circuit;
+        circuits[min_pair.1] = *next_circuit;
+        *next_circuit += 1;
+    } else if left_circuit != -1 && right_circuit == -1 {
+        // Add to left circuit
+        circuits[min_pair.1] = left_circuit;
+    } else if left_circuit == -1 && right_circuit != -1 {
+        // Add to right circuit
+        circuits[min_pair.0] = right_circuit;
+    } else {
+        // Merge circuits to left_circuit;
+        circuits.iter_mut().for_each(|circuit| {
+            if *circuit == right_circuit {
+                *circuit = left_circuit;
             }
-        }
-        progress.inc(1);
-        pairs.insert(min_pair);
-
-        let left_circuit = circuits[min_pair.0];
-        let right_circuit = circuits[min_pair.1];
-        if left_circuit == -1 && right_circuit == -1 {
-            // New circuit
-            circuits[min_pair.0] = next_circuit;
-            circuits[min_pair.1] = next_circuit;
-            next_circuit += 1;
-        } else if left_circuit != -1 && right_circuit == -1 {
-            // Add to left circuit
-            circuits[min_pair.1] = left_circuit;
-        } else if left_circuit == -1 && right_circuit != -1 {
-            // Add to right circuit
-            circuits[min_pair.0] = right_circuit;
-        } else {
-            // Merge circuits to left_circuit;
-            circuits.iter_mut().for_each(|circuit| {
-                if *circuit == right_circuit {
-                    *circuit = left_circuit;
-                }
-            });
-        }
+        });
     }
-    progress.finish_and_clear();
-    // count size of each circuit
-    let mut circuit_sizes = vec![0; (next_circuit) as usize];
-    for circuit in circuits.iter() {
-        if *circuit != -1 {
-            circuit_sizes[*circuit as usize] += 1;
-        }
-    }
-
-    // multiply sizes of three largest circuits
-    circuit_sizes.sort();
-    circuit_sizes.reverse();
-    return circuit_sizes.iter().take(3).product();
 }
 
-fn part2(filename: &str) -> usize {
-    // position, circuit index
+/// Playground
+/// connect light circuits by distance
+fn part(filename: &str, is_part1: bool) -> usize {
     let mut positions = Vec::<Position>::new();
     let mut circuits = Vec::<isize>::new();
-    // parse
     let lines = aoc::read_lines(filename);
     for line in lines {
         let xyz = aoc::parse_delimited::<i32>(&line, ',');
@@ -120,80 +57,61 @@ fn part2(filename: &str) -> usize {
         };
         positions.push(pos);
         circuits.push(-1);
-        // println!("{} {:?}", positions.len() - 1, pos);
     }
-    // create new progressbar with total number of positions
-    let progress = ProgressBar::new(100u64);
-    progress.set_style(ProgressStyle::with_template(aoc::STYLE).unwrap());
+    let mut distances = Vec::<((usize, usize), f64)>::new();
+    // compute all-to-all distances
+    for ldx in 0..positions.len() {
+        for rdx in (ldx + 1)..positions.len() {
+            let dist = positions[ldx].distance(&positions[rdx]);
+            distances.push(((ldx, rdx), dist));
+        }
+    }
+    // sort distance between nodes
+    distances.sort_by(|a, b| a.1.partial_cmp(&b.1).unwrap());
 
     let mut next_circuit = 0isize;
-    let mut pairs = HashSet::<(usize, usize)>::new();
-    loop {
-        let mut min_dist = std::f64::MAX;
-        let mut min_pair = (0usize, 0usize);
-        for ldx in 0..positions.len() {
-            for rdx in 0..positions.len() {
-                // println!("Considering pair: {} and {}", ldx, rdx);
-                if ldx <= rdx {
-                    continue;
-                }
-                if pairs.contains(&(ldx, rdx)) {
-                    continue;
-                }
-                if pairs.contains(&(rdx, ldx)) {
-                    continue;
-                }
-                let dist = positions[ldx].distance(&positions[rdx]);
-                // println!("Distance between {} and {}: {}", ldx, rdx, dist);
-                if dist < min_dist {
-                    min_dist = dist;
-                    min_pair = (ldx, rdx);
-                }
+
+    if is_part1 {
+        let num_connections = if filename.contains("train") { 10 } else { 1000 };
+        for ndx in 0..num_connections {
+            let min_pair = distances[ndx].0;
+            merge_circuits(&mut circuits, &mut next_circuit, min_pair);
+        }
+        // count size of each circuit
+        let mut circuit_sizes = vec![0; (next_circuit) as usize];
+        for circuit in circuits.iter() {
+            if *circuit != -1 {
+                circuit_sizes[*circuit as usize] += 1;
             }
         }
-        pairs.insert(min_pair);
-        let left_circuit = circuits[min_pair.0];
-        let right_circuit = circuits[min_pair.1];
-        if left_circuit == -1 && right_circuit == -1 {
-            // New circuit
-            circuits[min_pair.0] = next_circuit;
-            circuits[min_pair.1] = next_circuit;
-            next_circuit += 1;
-        } else if left_circuit != -1 && right_circuit == -1 {
-            // Add to left circuit
-            circuits[min_pair.1] = left_circuit;
-        } else if left_circuit == -1 && right_circuit != -1 {
-            // Add to right circuit
-            circuits[min_pair.0] = right_circuit;
-        } else {
-            // Merge circuits to left_circuit;
-            circuits.iter_mut().for_each(|circuit| {
-                if *circuit == right_circuit {
-                    *circuit = left_circuit;
-                }
-            });
+
+        // multiply sizes of three largest circuits
+        circuit_sizes.sort();
+        circuit_sizes.reverse();
+        return circuit_sizes.iter().take(3).product();
+    } else {
+        let mut num_circuits = usize::MAX;
+        let mut ndx = 0usize;
+        let mut min_pair = (0usize, 0usize);
+        while num_circuits > 1 {
+            min_pair = distances[ndx].0;
+            ndx += 1;
+            merge_circuits(&mut circuits, &mut next_circuit, min_pair);
+            let unique_circuits: HashSet<isize> = circuits.iter().cloned().collect();
+            num_circuits = unique_circuits.len();
         }
-        let unique_circuits: HashSet<isize> = circuits.iter().cloned().collect();
-        let num_circuits = unique_circuits.len();
-        if num_circuits < 100 {
-            progress.set_position(101 - unique_circuits.len() as u64);
-        }
-        progress.set_message(format!("num_circuits = {}", num_circuits));
-        if num_circuits == 1 {
-            // exit if only one circuit remains
-            progress.finish_and_clear();
-            return (positions[min_pair.0].x as usize) * (positions[min_pair.1].x as usize);
-        }
+        // only one circuit remains, compute x distance
+        return (positions[min_pair.0].x as usize) * (positions[min_pair.1].x as usize);
     }
 }
 
 /// Check training data, then apply to test data
 pub fn solve(day: usize) {
-    assert_eq!(part1(&format!("input/{:02}_train", day)), 40);
-    println!("Part1: {}", part1(&format!("input/{:02}_test", day)));
+    assert_eq!(part(&format!("input/{:02}_train", day), true), 40);
+    println!("Part1: {}", part(&format!("input/{:02}_test", day), true));
 
-    assert_eq!(part2(&format!("input/{:02}_train", day)), 25272);
-    println!("Part2: {}", part2(&format!("input/{:02}_test", day)));
+    assert_eq!(part(&format!("input/{:02}_train", day), false), 25272);
+    println!("Part2: {}", part(&format!("input/{:02}_test", day), false));
 
     println!("Coded: 107 minutes.");
 }
